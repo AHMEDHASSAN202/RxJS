afa9911a12f44e43ebb48ec231dde7e1e3e8e129
AHMEDHASSAN202 <ahmedhassanhassan707@gmail.com>
2019-01-18 00:39:08 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs';
+import { allBooks } from './data';
+
+let AllBooksObservable$ = new Observable(subscribe => {
+    
+    if (document.title == '') {
+        subscribe.error('please add title element');
+    }
+
+    for (let book of allBooks) {
+        subscribe.next(book);
+    }
+
+    setTimeout(() => {
+        subscribe.complete();
+    }, 2000);
+});
+
+AllBooksObservable$.subscribe(
+    data => {
+        console.log(data);
+    },
+    error => {
+        console.log(error);
+    },
+    () => {
+        console.log('complate async');
+    }
+);
+
